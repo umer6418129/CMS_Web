8b98d01c0e6391ac765ac72f1fd344ecded035c7
umer <md.farooq.ansari8959149@gmail.com>
2024-03-26 23:12:20 +0500

    work going on on a home page

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,17 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { AppConst } from './app.Const';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path:'',
+    loadChildren:()=>import("./pages/home/home.module").then((m)=>m.HomeModule)
+  },
+  {
+    path:AppConst.MAIN_ROUTES.Home,
+    loadChildren:()=>import("./pages/home/home.module").then((m)=>m.HomeModule)
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
