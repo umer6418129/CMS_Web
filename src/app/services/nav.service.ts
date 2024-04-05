1ff0153dbf585a1f99e978aad9b402bb8ce055fb
umer <md.farooq.ansari8959149@gmail.com>
2024-04-05 22:59:53 +0500

    work starting on depart

--- src/app/services/nav.service.ts
+++ src/app/services/nav.service.ts
@@ -2,30 +2,41 @@ import { Injectable } from '@angular/core';
 import { AppConst } from '../app.Const';
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class NavService {
   MENUITEMS: any[] = [
     {
       path: AppConst.MAIN_ROUTES.Home,
       title: 'Home',
+      isChild: false,
     },
     {
       path: AppConst.MAIN_ROUTES.ABOUT,
       title: 'the collage',
+      isChild: false,
     },
     {
       path: AppConst.MAIN_ROUTES.FACILITIES,
       title: 'facilities',
+      isChild: false,
     },
     {
       path: AppConst.MAIN_ROUTES.COURSES,
-      title: "Courses",
+      title: 'Courses',
+      isChild: false,
     },
     {
       path: AppConst.MAIN_ROUTES.CONTACT,
       title: 'contact us',
+      isChild: false,
+    },
+    {
+      path: '',
+      title: 'departments',
+      isChild: true,
+      childs : [],
     },
   ];
-  constructor() { }
+  constructor() {}
 }
